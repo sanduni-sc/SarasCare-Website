00bbe72dd6b98ed83f98e8b6f4eb6fc86d0c4a33
sanduni-sc <sanduni@saras.care>
2024-02-28 11:28:50 +1100

    Hosting updates 2802

---
+++ py_sarascare/sc_web/urls.py
@@ -0,0 +1,16 @@
+
+from django.urls import path
+from . import views
+
+urlpatterns = [
+    path('', views.home, name='home'),
+    path('causes/', views.causes, name='causes'),
+    path('portfolio/', views.portfolio, name='portfolio'),
+    path('gallery/', views.gallery, name='gallery'),
+    path('services/', views.services, name='services'),
+    path('blog/', views.blog, name='blog'),
+    path('contact-us/', views.contactus, name='contactus'),
+    path('404/', views.pagenotfound, name='404')
+    # Add more paths as needed
+]
+
